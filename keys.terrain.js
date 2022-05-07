7ec8e411f216b9d3aa69527ea987b26e75fe623e
Tosan <45423831+lodgendury@users.noreply.github.com>
2022-05-07 10:10:14 +0100

    Update keys.terrain.js

--- keys.terrain.js
+++ keys.terrain.js
@@ -1,6 +1,9 @@
 // can use `process.env.SECRET_MNEMONIC` or `process.env.SECRET_PRIV_KEY`
 // to populate secret in CI environment instead of hardcoding
 
+// We are configuring dotenv here 
+require('dotenv').config();
+
 module.exports = {
   custom_tester_1: {
     mnemonic:
@@ -8,5 +11,9 @@ module.exports = {
   },
   custom_tester_2: {
     privateKey: "fGl1yNoUnnNUqTUXXhxH9vJU0htlz9lWwBt3fQw+ixw=",
+  },
+  bombay: {
+    // Instead of the actual seed phrase we can use this :D
+    mnemonic: process.env.SEED_PHRASE,
   },
 };
