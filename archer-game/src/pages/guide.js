16c7f45ad7e4e447f6bdf12b557436847becefe7
Tosan <45423831+lodgendury@users.noreply.github.com>
2022-05-07 10:43:40 +0100

    Updated guide.js

--- archer-game/src/pages/guide.js
+++ archer-game/src/pages/guide.js
@@ -1,26 +1,73 @@
 import { Link } from 'react-router-dom';
+import { useWallet, WalletStatus } from "@terra-money/wallet-provider";
+import '../App.css';
+
 
 const Guide = () => {
+  const { status, connect, disconnect, availableConnectTypes } = useWallet();
+  console.log("Wallet status is ", status);
+  console.log("Available connection types:", availableConnectTypes);
+
+  const renderConnectButton = () => {
+    if (status === WalletStatus.WALLET_NOT_CONNECTED) {
+      
+      return (
+        <div className="connect-wallet-div">
+          <button
+            type="button"
+            key={`connect-EXTENSION`}
+            onClick={() => {connect("EXTENSION")}}
+            className="cta-button connect-wallet-button"
+          >
+            Connect Wallet to Play
+          </button>
+        </div>
+      );
+    }
+    else if (status === WalletStatus.WALLET_CONNECTED) {
+      return (
+        <button
+          type="button"
+          onClick={() => disconnect()}
+          className="cta-button disconnect-wallet-button"
+        >
+          Disconnect
+        </button>
+      );
+    }
+  };
+
   return (
     <main className="App">
       <header>
-        <Link to="/" className="home-link">
-          <div className="header-titles">
-            <h1>⚔ Goblin War ⚔️</h1>
-            <p>Only you can save us from Goblin town</p>
-          </div>
+      <Link to="/" >
+        <div className="header-titles">
+        <div class="estrellas inverso">
+  <span>R</span>
+  <span>E</span>
+  <span>H</span>
+  <span>C</span>
+  <span>R</span>
+  <span>A</span>
+</div>
+          
+        </div>
         </Link>
       </header>
 
-      <div className="score-board-container">
+      <div className="game-menu-container">
+      <div className="board-container">
         <h3>How to play</h3>
         
         <div>
           <span className="help">
-            Click as many goblins as you can within 15 seconds!
+            Kill as many bad guys as you can within 15 seconds!
           </span>
         </div>
+        <Link to="/" className="menu-button">HOME</Link>
       </div>
+      </div>
+      {renderConnectButton()}
     </main>
   );
 };
